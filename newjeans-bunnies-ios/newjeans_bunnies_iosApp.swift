4a81e0dddf788c6decab6c18e1cd489d0c75aeb6
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-28 09:14:53 +0900

    Initial Commit

---
+++ newjeans-bunnies-ios/newjeans_bunnies_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  newjeans_bunnies_iosApp.swift
+//  newjeans-bunnies-ios
+//
+//  Created by HamTory on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct newjeans_bunnies_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
