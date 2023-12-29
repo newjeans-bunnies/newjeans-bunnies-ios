e3b0f1fed4cb540244d52584bca61f07d06d868c
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-12-29 18:13:33 +0900

    :lipstick: :: add SplashScreen

---
+++ newjeans-bunnies-ios/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  newjeans-bunnies-ios
+//
+//  Created by HamTory on 12/28/23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Image("ic_app")
+            .resizable()
+            .frame(width:200, height: 92)
+    }
+}
+
+#Preview {
+    SplashView()
+}
