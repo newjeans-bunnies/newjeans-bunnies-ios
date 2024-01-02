af391802c9f2a33de37fc6404503beb4f3990cba
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-01-02 20:17:54 +0900

    :lipstick: :: add color

--- newjeans-bunnies-ios/designsystem/color/Color.swift
+++ newjeans-bunnies-ios/designsystem/color/Color.swift
@@ -12,9 +12,12 @@ import SwiftUI
 //text
 let FieldHintTextColor = Color(red: 167/255, green: 167/255, blue: 167/255)
 let FieldTextColor = Color(red: 66/255, green: 66/255, blue: 66/255)
-let ButtonTextColor = Color(red: 66/255.0, green: 66/255.0, blue: 66/255.0)
+let ButtonTextColor = Color(red: 66/255, green: 66/255, blue: 66/255)
 let ErrorTextColor = Color(red: 255/255, green: 94/255, blue: 94/255)
+let RadioButtonTextColor = Color(red: 0/255, green: 0/255, blue: 0/255)
+
 
 //background
 let FieldBackgroundColor = Color(red: 239/255, green: 239/255, blue: 239/255)
-let ButtonBackgroundColor = Color(red: 221/255.0, green: 221/255.0, blue: 221/255.0)
+let ButtonBackgroundColor = Color(red: 221/255, green: 221/255, blue: 221/255)
+let GroupRadioBackgroundColor = Color(red: 243/255, green: 243/255, blue: 243/255)
