6348c74be54022aecf66ba52d15510da1586ebae
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-01-02 20:17:19 +0900

    :fire: :: delect TopAppbar

--- newjeans-bunnies-ios/auth/ui/TopAppbar.swift
+++ newjeans-bunnies-ios/auth/ui/TopAppbar.swift
@@ -5,4 +5,30 @@
 //  Created by HamTory on 12/30/23.
 //
 
-import Foundation
+import SwiftUI
+import FlowKit
+
+
+struct TopAppbar: View {
+    @Flow var navigation
+    var body: some View{
+        HStack{
+            Button(action: {
+                navigation.pop()
+            }, label: {
+                Image("ic_arrow_back")
+                    .resizable()
+                    .frame(width:10, height: 18)
+            }).frame(alignment: .leading)
+            Text("회원가입")
+                .font(.custom(pretendardBold, size: 20))
+        }.frame(maxWidth: .infinity, maxHeight: 50,alignment: .center)
+        
+    }
+    
+}
+
+
+#Preview {
+    TopAppbar()
+}
