88bfd87dd1b4b86299ba909982ea5fea65a63b70
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-01-06 14:10:26 +0900

    :lipstick: :: add signup button, add ConditionsOfUse, update scrollView

--- newjeans-bunnies-ios/auth/view/Signup.swift
+++ newjeans-bunnies-ios/auth/view/Signup.swift
@@ -33,48 +33,242 @@ struct Signup: View {
     
     
     var body: some View {
-        VStack{
-            HStack{
+        VStack {
+            ZStack {
                 Button(action: {
                     navigation.pop()
                 }, label: {
                     Image("ic_arrow_back")
                         .resizable()
                         .frame(width:10, height: 18)
-                }).frame(alignment: .leading)
-                    
+                }).frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 30)
+                
                 Text("회원가입")
                     .font(.custom(pretendardBold, size: 20))
                 
             }.frame(maxWidth: .infinity, maxHeight: 50,alignment: .center)
-            
-            Text("아이디")
-                .font(.custom(pretendardSemiBold, size: 17))
-            
-            HStack(alignment: .center) {
-                TextField("아이디", text: $userId, prompt:Text("아이디").foregroundColor(Color("FieldHintTextColor")))
-                    .onChange(of: userId) { oldText, newText in
-                        if newText.count > userIdmaxCharacterCount {
-                            userId = String(newText.prefix(userIdmaxCharacterCount))
+            ScrollView(.vertical, showsIndicators: false){
+                Text("아이디")
+                    .font(.custom(pretendardSemiBold, size: 17))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                HStack(alignment: .center) {
+                    TextField("아이디", text: $userId, prompt:Text("아이디").foregroundColor(Color("FieldHintTextColor")))
+                        .onChange(of: userId) { oldText, newText in
+                            if newText.count > userIdmaxCharacterCount {
+                                userId = String(newText.prefix(userIdmaxCharacterCount))
+                            }
                         }
+                        .foregroundColor(Color("FieldTextColor"))
+                        .font(.custom(pretendardMedium, size: 18))
+                        .padding()
+                        .background(Color("FieldBackgroundColor"))
+                        .cornerRadius(13)
+                        .textContentType(.name)
+                        .keyboardType(.asciiCapable)
+                        .textInputAutocapitalization(.never)
+                    
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("중복 확인")
+                            .font(.custom(pretendardSemiBold, size: 17))
+                            .foregroundColor(Color("ButtonTextColor"))
+                            .frame(width: 100)
                     }
-                    .foregroundColor(Color("FieldTextColor"))
-                    .font(.custom(pretendardMedium, size: 18))
+                    )
+                    .controlSize(.large)
+                    .buttonStyle(PlainButtonStyle())
                     .padding()
-                    .background(Color("FieldBackgroundColor"))
+                    .background(Color("ButtonBackgroundColor"))
                     .cornerRadius(13)
-                    .textContentType(.name)
-                    .keyboardType(.asciiCapable)
-                    .textInputAutocapitalization(.never)
+                }
                 
+                
+                Text("비밀번호")
+                    .font(.custom(pretendardSemiBold, size: 17))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 30)
+                
+                HStack(alignment: .center) {
+                    if passwordHideStatus{
+                        SecureField("비밀번호", text: $password, prompt:Text("비밀번호")
+                            .foregroundColor(Color("FieldHintTextColor"))
+                            .font(.custom(pretendardMedium, size: 18))
+                        )
+                        .foregroundColor(Color("FieldTextColor"))
+                        .textInputAutocapitalization(.never)
+                        .textContentType(.password)
+                        .keyboardType(.asciiCapable)
+                    } else {
+                        TextField("비밀번호", text: $password, prompt:Text("비밀번호")
+                            .foregroundColor(Color("FieldHintTextColor"))
+                            .font(.custom(pretendardMedium, size: 18))
+                        )
+                        .foregroundColor(Color("FieldTextColor"))
+                        .textInputAutocapitalization(.never)
+                        .textContentType(.password)
+                        .keyboardType(.asciiCapable)
+                    }
+                    
+                    Toggle(isOn: $passwordHideStatus){}
+                        .toggleStyle(PasswordHideToggleStyle())
+                        .padding(.trailing, 9)
+                    
+                }
+                .padding()
+                .background(Color("FieldBackgroundColor"))
+                .cornerRadius(13)
+                
+                HStack(alignment: .center) {
+                    if checkPasswordHideStatus{
+                        SecureField("비밀번호 확인", text: $checkPassword, prompt:Text("비밀번호 확인")
+                            .foregroundColor(Color("FieldHintTextColor"))
+                            .font(.custom(pretendardMedium, size: 18))
+                        )
+                        .foregroundColor(Color("FieldTextColor"))
+                        .textInputAutocapitalization(.never)
+                        .textContentType(.password)
+                        .keyboardType(.asciiCapable)
+                    } else {
+                        TextField("비밀번호 확인", text: $checkPassword, prompt:Text("비밀번호 확인")
+                            .foregroundColor(Color("FieldHintTextColor"))
+                            .font(.custom(pretendardMedium, size: 18))
+                        )
+                        .foregroundColor(Color("FieldTextColor"))
+                        .textInputAutocapitalization(.never)
+                        .textContentType(.password)
+                        .keyboardType(.asciiCapable)
+                    }
+                    
+                    Toggle(isOn: $checkPasswordHideStatus){}
+                        .toggleStyle(PasswordHideToggleStyle())
+                        .padding(.trailing, 9)
+                    
+                }
+                .padding()
+                .background(Color("FieldBackgroundColor"))
+                .cornerRadius(13)
+                
+                Text("전화번호")
+                    .font(.custom(pretendardSemiBold, size: 17))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 30)
+                
+                HStack(alignment: .center) {
+                    TextField("전화번호", text: $phoneNumber, prompt:Text("전화번호").foregroundColor(Color("FieldHintTextColor")))
+                        .onChange(of: phoneNumber) { oldText, newText in
+                            if newText.count > phoneNumbermaxCharacterCount {
+                                phoneNumber = String(newText.prefix(phoneNumbermaxCharacterCount))
+                            }
+                        }
+                        .foregroundColor(Color("FieldTextColor"))
+                        .font(.custom(pretendardMedium, size: 18))
+                        .padding()
+                        .background(Color("FieldBackgroundColor"))
+                        .cornerRadius(13)
+                        .textContentType(.telephoneNumber)
+                        .keyboardType(.phonePad)
+                    
+                    
+                    Button(action: {}, label: {
+                        Text("인증번호 받기")
+                            .font(.custom(pretendardSemiBold, size: 17))
+                            .foregroundColor(Color("ButtonTextColor"))
+                            .frame(width: 100)
+                    }
+                    )
+                    .controlSize(.large)
+                    .buttonStyle(PlainButtonStyle())
+                    .padding()
+                    .background(Color("ButtonBackgroundColor"))
+                    .cornerRadius(13)
+                }
+                
+                HStack(alignment: .center) {
+                    TextField("인증번호", text: $certificationNumber, prompt:Text("인증번호")
+                        .foregroundColor(Color("FieldHintTextColor")))
+                        .onChange(of: certificationNumber) { oldText, newText in
+                            if newText.count > certificationNumbermaxCharacterCount {
+                                certificationNumber = String(newText.prefix(certificationNumbermaxCharacterCount))
+                            }
+                        }
+                        .foregroundColor(Color("FieldTextColor"))
+                        .font(.custom(pretendardMedium, size: 18))
+                        .padding()
+                        .background(Color("FieldBackgroundColor"))
+                        .cornerRadius(13)
+                        .textContentType(.oneTimeCode)
+                        .keyboardType(.numberPad)
+                    
+                    Button(action: {}, label: {
+                        Text("확인")
+                            .font(.custom(pretendardSemiBold, size: 17))
+                            .foregroundColor(Color("ButtonTextColor"))
+                            .frame(width: 100)
+                    }
+                    )
+                    .controlSize(.large)
+                    .buttonStyle(PlainButtonStyle())
+                    .padding()
+                    .background(Color("ButtonBackgroundColor"))
+                    .cornerRadius(13)
+                }
+                
+                Text("나라")
+                    .font(.custom(pretendardSemiBold, size: 17))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 30)
+                
+                HStack(spacing:7) {
+                    ForEach(items, id: \.self) { country in
+                        SelectCountryRadioButton(text: country)
+                    }
+                }
+                .padding(.vertical, 5)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .background(Color("GroupRadioBackgroundColor"))
+                .cornerRadius(13)
+                
+                
+                Text("아래 약관에 모두 동의 합니다")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.custom(pretendardBold, size: 16))
+                    .padding(.top, 20)
+                    .padding(.bottom, 8)
+                HStack{
+                    Toggle(isOn: $viewModel.useAgreementStatus){}
+                        .toggleStyle(CheckBoxToggleStyle())
+                    Text("이용약관 동의")
+                        .font(.custom(pretendardRegular, size: 17))
+                        .padding(.leading, 10)
+                    Text("보기 >")
+                        .font(.custom(pretendardRegular, size: 17))
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                HStack{
+                    Toggle(isOn: $viewModel.informationConsentStatus){}
+                        .toggleStyle(CheckBoxToggleStyle())
+                    Text("개인정보 취급방침 동의")
+                        .font(.custom(pretendardRegular, size: 17))
+                        .padding(.leading, 10)
+                    Text("보기 >")
+                        .font(.custom(pretendardRegular, size: 17))
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+
+                }.frame(maxWidth: .infinity, alignment: .leading)                        .lineLimit(1)
+                    .padding(.bottom, 20)
                 
                 Button(action: {
-                    
+                    viewModel.signup()
                 }, label: {
-                    Text("중복 확인")
+                    Text("계정 만들기")
                         .font(.custom(pretendardBold, size: 17))
                         .foregroundColor(Color("ButtonTextColor"))
-                        .frame(width: 100)
+                        .frame(maxWidth: .infinity)
                 }
                 )
                 .controlSize(.large)
@@ -82,166 +276,12 @@ struct Signup: View {
                 .padding()
                 .background(Color("ButtonBackgroundColor"))
                 .cornerRadius(13)
-            }
-            
-            
-            Text("비밀번호")
-                .font(.custom(pretendardSemiBold, size: 17))
-            
-            HStack(alignment: .center) {
-                if passwordHideStatus{
-                    SecureField("비밀번호", text: $password, prompt:Text("비밀번호")
-                        .foregroundColor(Color("FieldHintTextColor"))
-                        .font(.custom(pretendardMedium, size: 18))
-                    )
-                    .foregroundColor(Color("FieldTextColor"))
-                    .textInputAutocapitalization(.never)
-                    .textContentType(.password)
-                    .keyboardType(.asciiCapable)
-                } else {
-                    TextField("비밀번호", text: $password, prompt:Text("비밀번호")
-                        .foregroundColor(Color("FieldHintTextColor"))
-                        .font(.custom(pretendardMedium, size: 18))
-                    )
-                    .foregroundColor(Color("FieldTextColor"))
-                    .textInputAutocapitalization(.never)
-                    .textContentType(.password)
-                    .keyboardType(.asciiCapable)
-                }
-                
-                Toggle(isOn: $passwordHideStatus){}
-                    .toggleStyle(PasswordHideToggleStyle())
-                    .padding(.trailing, 9)
+                .padding(.bottom, 10)
                 
             }
-            .padding()
-            .background(Color("FieldBackgroundColor"))
-            .cornerRadius(13)
-            
-            HStack(alignment: .center) {
-                if checkPasswordHideStatus{
-                    SecureField("비밀번호 확인", text: $checkPassword, prompt:Text("비밀번호 확인")
-                        .foregroundColor(Color("FieldHintTextColor"))
-                        .font(.custom(pretendardMedium, size: 18))
-                    )
-                    .foregroundColor(Color("FieldTextColor"))
-                    .textInputAutocapitalization(.never)
-                    .textContentType(.password)
-                    .keyboardType(.asciiCapable)
-                } else {
-                    TextField("비밀번호 확인", text: $checkPassword, prompt:Text("비밀번호 확인")
-                        .foregroundColor(Color("FieldHintTextColor"))
-                        .font(.custom(pretendardMedium, size: 18))
-                    )
-                    .foregroundColor(Color("FieldTextColor"))
-                    .textInputAutocapitalization(.never)
-                    .textContentType(.password)
-                    .keyboardType(.asciiCapable)
-                }
-                
-                Toggle(isOn: $checkPasswordHideStatus){}
-                    .toggleStyle(PasswordHideToggleStyle())
-                    .padding(.trailing, 9)
-                
-            }
-            .padding()
-            .background(Color("FieldBackgroundColor"))
-            .cornerRadius(13)
-            
-            Text("전화번호")
-                .font(.custom(pretendardSemiBold, size: 17))
-            
-            HStack(alignment: .center) {
-                TextField("전화번호", text: $phoneNumber, prompt:Text("전화번호").foregroundColor(Color("FieldHintTextColor")))
-                    .onChange(of: phoneNumber) { oldText, newText in
-                        if newText.count > phoneNumbermaxCharacterCount {
-                            phoneNumber = String(newText.prefix(phoneNumbermaxCharacterCount))
-                        }
-                    }
-                    .foregroundColor(Color("FieldTextColor"))
-                    .font(.custom(pretendardMedium, size: 18))
-                    .padding()
-                    .background(Color("FieldBackgroundColor"))
-                    .cornerRadius(13)
-                    .textContentType(.telephoneNumber)
-                    .keyboardType(.phonePad)
-                
-                
-                Button(action: {}, label: {
-                    Text("인증번호 받기")
-                        .font(.custom(pretendardBold, size: 17))
-                        .foregroundColor(Color("ButtonTextColor"))
-                        .frame(width: 100)
-                }
-                )
-                .controlSize(.large)
-                .buttonStyle(PlainButtonStyle())
-                .padding()
-                .background(Color("ButtonBackgroundColor"))
-                .cornerRadius(13)
-            }
-            
-            HStack(alignment: .center) {
-                TextField("전화번호 인증", text: $certificationNumber, prompt:Text("전화번호 인증")                    .foregroundColor(Color("FieldHintTextColor")))
-                
-                    .onChange(of: certificationNumber) { oldText, newText in
-                        if newText.count > certificationNumbermaxCharacterCount {
-                            certificationNumber = String(newText.prefix(certificationNumbermaxCharacterCount))
-                        }
-                    }
-                    .foregroundColor(Color("FieldTextColor"))
-                    .font(.custom(pretendardMedium, size: 18))
-                    .padding()
-                    .background(Color("FieldBackgroundColor"))
-                    .cornerRadius(13)
-                    .textContentType(.oneTimeCode)
-                    .keyboardType(.numberPad)
-                
-                Button(action: {}, label: {
-                    Text("확인")
-                        .font(.custom(pretendardBold, size: 17))
-                        .foregroundColor(Color("ButtonTextColor"))
-                        .frame(width: 100)
-                }
-                )
-                .controlSize(.large)
-                .buttonStyle(PlainButtonStyle())
-                .padding()
-                .background(Color("ButtonBackgroundColor"))
-                .cornerRadius(13)
-            }
-            
-            Text("나라")
-                .font(.custom(pretendardSemiBold, size: 17))
-            
-            HStack(spacing:7) {
-                ForEach(items, id: \.self) { country in
-                    SelectCountryRadioButton(text: country)
-                }
-            }
-            .padding(.vertical, 5)
-            .frame(maxWidth: .infinity, alignment: .center)
-            .background(Color("GroupRadioBackgroundColor"))
-            .cornerRadius(13)
-            
-            Button(action: {
-                
-            }, label: {
-                Text("계정 만들기")
-                    .font(.custom(pretendardBold, size: 17))
-                    .foregroundColor(Color("ButtonTextColor"))
-                    .frame(maxWidth: .infinity)
-            }
-            )
-            .controlSize(.large)
-            .buttonStyle(PlainButtonStyle())
-            .padding()
-            .background(Color("ButtonBackgroundColor"))
-            .cornerRadius(13)
-            
+            .padding(.horizontal, 30)
+            .navigationBarHidden(true)
         }
-        .padding(.horizontal, 30)
-        .navigationBarHidden(true)
     }
 }
 
