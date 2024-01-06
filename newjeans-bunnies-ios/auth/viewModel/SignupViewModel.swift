909fb5fcc38d84222b95cfa6ec727dae8cc03220
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-01-06 14:11:55 +0900

    :zap: :: add informationConsentStatus, useAgreementStatus

--- newjeans-bunnies-ios/auth/viewModel/SignupViewModel.swift
+++ newjeans-bunnies-ios/auth/viewModel/SignupViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftUI
+
+class SignupViewModel: ObservableObject {
+    
+    @Published var country: String = "KR"
+    
+    //이용약관 동의
+    @Published var useAgreementStatus: Bool = false
+    
+    //개인정보 취급방침 동의
+    @Published var informationConsentStatus: Bool = false
+    
+    
+    func signup(){
+        
+    }
+    
+}
